490e732807b944356909a1768b7cb1cdc45968d0
SanyamPunia <lewarends@gmail.com>
2022-10-22 15:39:19 +0530

    add 'rel' on Links

--- storefront/modules/components/Hero.tsx
+++ storefront/modules/components/Hero.tsx
@@ -1,11 +1,23 @@
 import Login from "@modules/account/login";
+import Link from "next/link";
 
 const Hero = () => {
   return (
     <div className="container mx-auto text-center flex flex-col gap-3">
       <h1 className="font-playfair text-4xl">Auth Starter Template</h1>
       <p className="font-notosans text-sm text-gray-500">
-        Made using <a className="text-red-400 hover:text-red-700" href="https://medusajs.com/" target="_blank">Medusa.js</a>, <a className="text-red-400 hover:text-red-700" href="https://nextjs.org/" target="_blank">Next.js</a> & <a className="text-red-400 hover:text-red-700" href="https://tailwindcss.com/" target="_blank">Tailwind CSS</a>
+        Made using{" "}
+        <Link target="_blank" rel="noreferrer" href="https://medusajs.com/">
+          <a className="text-red-400 hover:text-red-700">Medusa.js</a>
+        </Link>
+        ,{" "}
+        <Link target="_blank" rel="noreferrer" href="https://nextjs.org/">
+          <a className="text-red-400 hover:text-red-700">Next.js</a>
+        </Link>{" "}
+        &{" "}
+        <Link target="_blank" rel="noreferrer" href="https://tailwindcss.com/">
+          <a className="text-red-400 hover:text-red-700">Tailwind CSS</a>
+        </Link>
       </p>
       <Login />
     </div>
